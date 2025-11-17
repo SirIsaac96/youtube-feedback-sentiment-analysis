2bbe18a74c14c1bb7114a4a91540473aeb0229be
Isaac Otom <isaacotom@gmail.com>
2025-11-17 22:15:30 +0300

    Added cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze viewer comments to gain insights into viewer feedback; whether a feedback is positive, neutral, or negative.',
+    author='Isaac-Otom',
+    license='',
+)
